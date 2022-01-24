b261f83161e66e27a3276f73b0ee723624c162aa
slin69 <83825913+slin69@users.noreply.github.com>
2022-01-24 16:30:32 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, make_response, redirect
+from flask_socketio import SocketIO, send, emit
+import os
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on("message")
+def handleMessage(data):
+    emit("new_message",data,broadcast=True)
+    
+if __name__ == "__main__":
+    socketio.run(app, debug=True, port=5004)
